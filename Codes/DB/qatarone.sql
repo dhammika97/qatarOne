8bbcadce7bc7d67d84bf12e73d94dc8741e4653f
Dhammika <dhammika97@gmail.com>
2014-11-19 00:45:59 +0530

    database 1.0.1

--- Codes/DB/qatarone.sql
+++ Codes/DB/qatarone.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2014 at 06:23 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: Nov 18, 2014 at 08:14 PM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -59,20 +59,13 @@ CREATE TABLE IF NOT EXISTS `advertisment` (
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `category_id` int(11) NOT NULL,
-  `category_name` varchar(50) NOT NULL,
-  `category_entereddate` datetime NOT NULL,
-  `category_enteredby` varchar(100) NOT NULL,
-  `category_status` varchar(3) NOT NULL
+  `category_id` int(5) NOT NULL,
+  `category_name` varchar(250) NOT NULL,
+  `category_enteredDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `category_enteredBy` int(11) NOT NULL,
+  `category_status` int(1) NOT NULL,
+  PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`category_id`, `category_name`, `category_entereddate`, `category_enteredby`, `category_status`) VALUES
-(2, 'Van', '2014-11-14 00:00:00', 'usama', 'new'),
-(1, 'Car', '2014-11-15 00:00:00', 'usama', 'new');
 
 -- --------------------------------------------------------
 
@@ -81,11 +74,13 @@ INSERT INTO `category` (`category_id`, `category_name`, `category_entereddate`, 
 --
 
 CREATE TABLE IF NOT EXISTS `category_sub` (
-  `category_sub_name` varchar(20) NOT NULL,
-  `category_sub_entereddate` datetime NOT NULL,
-  `category_sub_enteredby` varchar(10) NOT NULL,
-  `category_sub_status` varchar(10) NOT NULL,
-  `category_sub_categoryid` int(11) NOT NULL
+  `category_sub_id` int(4) NOT NULL,
+  `category_sub_name` varchar(250) NOT NULL,
+  `category_sub_enteredDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `category_sub_enteredBy` int(5) NOT NULL,
+  `category_sub_status` int(1) NOT NULL,
+  `category_sub_parentId` int(4) NOT NULL,
+  PRIMARY KEY (`category_sub_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -115,15 +110,22 @@ CREATE TABLE IF NOT EXISTS `comments` (
 CREATE TABLE IF NOT EXISTS `event` (
   `event_id` int(11) NOT NULL,
   `event_type` varchar(10) NOT NULL,
+  `event_eventDate` datetime NOT NULL,
   `event_description` varchar(100) NOT NULL,
-  `event_note` varchar(100) NOT NULL,
   `event_imageurl` varchar(100) NOT NULL,
   `event_videourl` varchar(100) NOT NULL,
-  `event_entereddate` datetime NOT NULL,
-  `event_enteredby` varchar(10) NOT NULL,
-  `event_approvedstatus` varchar(10) NOT NULL,
-  `event_status` varchar(10) NOT NULL
+  `event_enteredDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `event_enteredBy` int(5) NOT NULL,
+  `event_status` int(1) NOT NULL,
+  PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`event_id`, `event_type`, `event_eventDate`, `event_description`, `event_imageurl`, `event_videourl`, `event_enteredDate`, `event_enteredBy`, `event_status`) VALUES
+(0, 'xcv', '0000-00-00 00:00:00', 'xcv', '', '', '2014-11-04 18:30:00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -132,37 +134,34 @@ CREATE TABLE IF NOT EXISTS `event` (
 --
 
 CREATE TABLE IF NOT EXISTS `fixedads` (
-`fixedads_id` int(11) NOT NULL,
-  `fixedads_type` varchar(10) NOT NULL,
-  `fixedads_description` varchar(100) NOT NULL,
-  `fixedads_note` varchar(100) NOT NULL,
+  `fixedads_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fixedads_type` int(2) NOT NULL,
   `fixedads_imageurl` varchar(100) NOT NULL,
-  `fixedads_videourl` varchar(100) NOT NULL,
-  `fixedads_enetreddate` datetime NOT NULL,
-  `fixedads_enteredby` varchar(10) NOT NULL,
-  `fixedads_approvedstatus` varchar(10) NOT NULL,
-  `fixedads_status` varchar(10) NOT NULL
+  `fixedads_enetredDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fixedads_enteredBy` int(10) NOT NULL,
+  `fixedads_status` int(1) NOT NULL,
+  PRIMARY KEY (`fixedads_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `fixedads`
 --
 
-INSERT INTO `fixedads` (`fixedads_id`, `fixedads_type`, `fixedads_description`, `fixedads_note`, `fixedads_imageurl`, `fixedads_videourl`, `fixedads_enetreddate`, `fixedads_enteredby`, `fixedads_approvedstatus`, `fixedads_status`) VALUES
-(1, 'type one', 'All the fixed add would come here', 'fixed ad test note', '', '', '2014-11-17 00:00:00', 'test user', 'apprd    ', 'apprd'),
-(2, 'TypeA', '', '', '', '', '0000-00-00 00:00:00', '', '', ''),
-(3, 'TypeA', 'Test Description', '', '', '', '0000-00-00 00:00:00', '', '', ''),
-(4, 'TypeA', 'Test Description', 'Test fixed Ad note', '', '', '0000-00-00 00:00:00', '', '', ''),
-(5, 'TypeA', 'Test Description', 'Test fixed Ad note', '', '', '0000-00-00 00:00:00', '', '', ''),
-(6, 'TypeA', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', '', ''),
-(7, 'TypeA', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', ''),
-(8, 'TypeA', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted'),
-(9, 'TypeB', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted'),
-(10, 'TypeC', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted'),
-(11, 'TypeC', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted'),
-(12, 'TypeC', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted'),
-(13, 'TypeC', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted'),
-(14, 'TypecxCcxs', 'Test Description', 'Test fixed Ad note', '', '', '2014-11-17 00:00:00', 'Clifford', 'Approved', 'Deleted');
+INSERT INTO `fixedads` (`fixedads_id`, `fixedads_type`, `fixedads_imageurl`, `fixedads_enetredDate`, `fixedads_enteredBy`, `fixedads_status`) VALUES
+(1, 0, '', '2014-11-16 18:30:00', 0, 0),
+(2, 0, '', '0000-00-00 00:00:00', 0, 0),
+(3, 0, '', '0000-00-00 00:00:00', 0, 0),
+(4, 0, '', '0000-00-00 00:00:00', 0, 0),
+(5, 0, '', '0000-00-00 00:00:00', 0, 0),
+(6, 0, '', '2014-11-16 18:30:00', 0, 0),
+(7, 0, '', '2014-11-16 18:30:00', 0, 0),
+(8, 0, '', '2014-11-16 18:30:00', 0, 0),
+(9, 0, '', '2014-11-16 18:30:00', 0, 0),
+(10, 0, '', '2014-11-16 18:30:00', 0, 0),
+(11, 0, '', '2014-11-16 18:30:00', 0, 0),
+(12, 0, '', '2014-11-16 18:30:00', 0, 0),
+(13, 0, '', '2014-11-16 18:30:00', 0, 0),
+(14, 0, '', '2014-11-16 18:30:00', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -218,7 +217,7 @@ CREATE TABLE IF NOT EXISTS `payment` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_username` varchar(50) NOT NULL,
   `user_password` varchar(20) NOT NULL,
   `user_email` varchar(50) NOT NULL,
@@ -230,53 +229,23 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_telephoneno1` varchar(20) NOT NULL,
   `user_telephoneno2` varchar(20) NOT NULL,
   `user_imageurl` varchar(50) NOT NULL,
-  `user_entereddate` datetime NOT NULL,
+  `user_entereddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_type` varchar(3) NOT NULL,
   `user_status` varchar(1) NOT NULL,
   `user_paymentstatus` varchar(1) NOT NULL,
   `user_category` varchar(20) NOT NULL,
-  `user_profiletype` varchar(20) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10004 ;
+  `user_profiletype` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `user_username`, `user_password`, `user_email`, `user_fullname`, `user_city`, `user_country`, `user_address1`, `user_address2`, `user_telephoneno1`, `user_telephoneno2`, `user_imageurl`, `user_entereddate`, `user_type`, `user_status`, `user_paymentstatus`, `user_category`, `user_profiletype`) VALUES
-(10001, 'Clifford', 'Clifford 45', 'Regis005', 'Tour Coordinator', 'Regis005', 'Regis005', 'Regis005', 'Regis005', 'Regis005', 'Regis005', 'Regis005', '0000-00-00 00:00:00', 'Reg', 'R', 'R', 'Regis005', 'jkjkj'),
-(10002, 'Clifford', 'Clifford 44', 'Regis005', 'Tour Coordinator', 'Regis005', 'Autralia', 'Park view', 'Sydney', '0114555', '0125666', 'Regis005', '0000-00-00 00:00:00', 'Reg', 'R', 'R', 'Regis005', 'jkjkj'),
-(10003, 'Steven', 'ste44', 'steve@gmail.com', 'Steven stanisylous', 'Perth', 'Australia', 'Park View', 'Perth', '011145522', '012255266', '', '2014-11-17 00:00:00', 'Typ', 'A', 'A', 'CategoryA', 'ProfOne');
+(1, 'sdf', 'sdf', 'sdf', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', '', '', '', '', ''),
+(2, 'xcc', 'xcvxcv', 'xcvxcv', '', '', '', '', '', '', '', '', '2014-11-18 18:05:08', '', '', '', '', '');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `fixedads`
---
-ALTER TABLE `fixedads`
- ADD PRIMARY KEY (`fixedads_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `fixedads`
---
-ALTER TABLE `fixedads`
-MODIFY `fixedads_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10004;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
