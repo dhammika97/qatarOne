873e56a95824e59c263051753497080c1a51c1f2
Dhammika <dhammika97@gmail.com>
2014-11-20 00:53:53 +0530

    ddatabase script update

--- Codes/DB/qatarone.sql
+++ Codes/DB/qatarone.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2014 at 07:48 PM
+-- Generation Time: Nov 19, 2014 at 08:22 PM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -23,13 +23,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `advertisement_images`
+--
+
+CREATE TABLE IF NOT EXISTS `advertisement_images` (
+  `advertisement_imageId` int(11) NOT NULL AUTO_INCREMENT,
+  `advertisement_id` int(11) NOT NULL,
+  `advertisement_image` varchar(255) NOT NULL,
+  PRIMARY KEY (`advertisement_imageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `advertisment`
 --
 
 CREATE TABLE IF NOT EXISTS `advertisment` (
-  `advertisment_id` int(11) NOT NULL,
+  `advertisment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `advertisement_categoryId` int(5) NOT NULL,
+  `advertisement_subCategoryId` int(5) NOT NULL,
+  `advertisement_attributes` longtext NOT NULL,
+  `advertisement_title` varchar(255) NOT NULL,
+  `advertisement_description` longtext NOT NULL,
+  `advertisement_price` decimal(10,0) NOT NULL,
+  `advertisement_contactName` varchar(255) NOT NULL,
+  `advertisement_contactNo` varchar(20) NOT NULL,
+  `advertisement_contactEmail` varchar(100) NOT NULL,
+  `advertisement_location` int(5) NOT NULL,
+  `advertisement_suburb` int(5) NOT NULL,
+  `advertisement_googleCodes` varchar(20) NOT NULL,
+  `advertisement_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `advertisement_status` int(1) NOT NULL,
+  `advertisement_expire` datetime NOT NULL,
   PRIMARY KEY (`advertisment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -44,6 +72,19 @@ CREATE TABLE IF NOT EXISTS `category` (
   `category_enteredBy` int(11) NOT NULL,
   `category_status` int(1) NOT NULL,
   PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categoryattributes`
+--
+
+CREATE TABLE IF NOT EXISTS `categoryattributes` (
+  `attribute_id` int(4) NOT NULL AUTO_INCREMENT,
+  `attribute_categoryId` int(4) NOT NULL,
+  `attributes` longtext NOT NULL,
+  PRIMARY KEY (`attribute_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -77,59 +118,7 @@ CREATE TABLE IF NOT EXISTS `fixedads` (
   `fixedads_enteredBy` int(10) NOT NULL,
   `fixedads_status` int(1) NOT NULL,
   PRIMARY KEY (`fixedads_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
-
---
--- Dumping data for table `fixedads`
---
-
-INSERT INTO `fixedads` (`fixedads_id`, `fixedads_type`, `fixedads_imageurl`, `fixedads_enetredDate`, `fixedads_enteredBy`, `fixedads_status`) VALUES
-(1, 0, '', '2014-11-16 18:30:00', 0, 0),
-(2, 0, '', '0000-00-00 00:00:00', 0, 0),
-(3, 0, '', '0000-00-00 00:00:00', 0, 0),
-(4, 0, '', '0000-00-00 00:00:00', 0, 0),
-(5, 0, '', '0000-00-00 00:00:00', 0, 0),
-(6, 0, '', '2014-11-16 18:30:00', 0, 0),
-(7, 0, '', '2014-11-16 18:30:00', 0, 0),
-(8, 0, '', '2014-11-16 18:30:00', 0, 0),
-(9, 0, '', '2014-11-16 18:30:00', 0, 0),
-(10, 0, '', '2014-11-16 18:30:00', 0, 0),
-(11, 0, '', '2014-11-16 18:30:00', 0, 0),
-(12, 0, '', '2014-11-16 18:30:00', 0, 0),
-(13, 0, '', '2014-11-16 18:30:00', 0, 0),
-(14, 0, '', '2014-11-16 18:30:00', 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `job`
---
-
-CREATE TABLE IF NOT EXISTS `job` (
-  `job_id` int(11) NOT NULL,
-  `job_phoneno1` varchar(20) NOT NULL,
-  `job_phoneno2` varchar(20) NOT NULL,
-  `job_phoneno_display` varchar(1) NOT NULL,
-  `job_locationid` int(11) NOT NULL,
-  `job_cityid` int(11) NOT NULL,
-  `job_imageurl` varchar(100) NOT NULL,
-  `job_jobtype` varchar(10) NOT NULL,
-  `job_jobcompanywebsite` varchar(50) NOT NULL,
-  `job_jobapplyvia` varchar(20) NOT NULL,
-  `job_salary` decimal(10,0) NOT NULL,
-  `job_salarytype` varchar(20) NOT NULL,
-  `job_salarynegotiable` varchar(20) NOT NULL,
-  `job_title` varchar(100) NOT NULL,
-  `job_description` varchar(1000) NOT NULL,
-  `job_rating_like` int(11) NOT NULL,
-  `job_rating_dislike` int(11) NOT NULL,
-  `rating_commentid` int(11) NOT NULL,
-  `job_entereddate` datetime NOT NULL,
-  `job_eneteredby` varchar(10) NOT NULL,
-  `job_approvedstatus` varchar(10) NOT NULL,
-  `job_status` varchar(10) NOT NULL,
-  `job_categorysubid` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -167,32 +156,24 @@ CREATE TABLE IF NOT EXISTS `suburbs` (
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_username` varchar(50) NOT NULL,
-  `user_password` varchar(20) NOT NULL,
+  `user_password` text NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `user_fullname` varchar(200) NOT NULL,
   `user_city` varchar(50) NOT NULL,
-  `user_country` varchar(50) NOT NULL,
+  `user_country` varchar(100) NOT NULL,
   `user_address1` varchar(100) NOT NULL,
   `user_address2` varchar(100) NOT NULL,
   `user_telephoneno1` varchar(20) NOT NULL,
   `user_telephoneno2` varchar(20) NOT NULL,
   `user_imageurl` varchar(50) NOT NULL,
   `user_entereddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_type` varchar(3) NOT NULL,
-  `user_status` varchar(1) NOT NULL,
-  `user_paymentstatus` varchar(1) NOT NULL,
-  `user_category` varchar(20) NOT NULL,
-  `user_profiletype` varchar(20) NOT NULL,
+  `user_type` int(1) NOT NULL,
+  `user_status` int(1) NOT NULL,
+  `user_paymentstatus` int(1) NOT NULL,
+  `user_category` int(1) NOT NULL,
+  `user_profiletype` int(1) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_username`, `user_password`, `user_email`, `user_fullname`, `user_city`, `user_country`, `user_address1`, `user_address2`, `user_telephoneno1`, `user_telephoneno2`, `user_imageurl`, `user_entereddate`, `user_type`, `user_status`, `user_paymentstatus`, `user_category`, `user_profiletype`) VALUES
-(1, 'sdf', 'sdf', 'sdf', '', '', '', '', '', '', '', '', '0000-00-00 00:00:00', '', '', '', '', ''),
-(2, 'xcc', 'xcvxcv', 'xcvxcv', '', '', '', '', '', '', '', '', '2014-11-18 18:05:08', '', '', '', '', '');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
