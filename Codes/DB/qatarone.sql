e8780280fed9e2f86189e33ff0fe95f5ed7f1f07
Dhammika <dhammika97@gmail.com>
2014-11-20 23:45:32 +0530

    database script update

--- Codes/DB/qatarone.sql
+++ Codes/DB/qatarone.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2014 at 06:28 PM
+-- Generation Time: Nov 20, 2014 at 07:14 PM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
