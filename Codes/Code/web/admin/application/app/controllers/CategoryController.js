e5f7aaf13e2fc22821c194af8cfdfdac8d877ae5
deegha <shuboothi@gmail.com>
2014-11-29 13:56:43 +0530

    delete category

--- Codes/Code/web/admin/application/app/controllers/CategoryController.js
+++ Codes/Code/web/admin/application/app/controllers/CategoryController.js
@@ -2,12 +2,12 @@
 controllers.categoryController = function($scope, categoryFactry){
 	$scope.test="safds";
 	$scope.categories = categoryFactry.getCategory();	
-	$scope.deleteUser = function(id){	
+	$scope.deleteCategory = function(id){	
 		if(id!=''){
 			var r = confirm("Do you want to delete this user!");
 			if (r == true) {
 				//$scope.users = usersFactory.deleteUser(id)
-				//categoryFactry.deleteCategory($scope,id)
+				categoryFactry.deleteCategory($scope,id)
 			}
 		}
 	}
