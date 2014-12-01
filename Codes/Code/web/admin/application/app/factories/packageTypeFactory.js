f426c9f04a2c05e1681eab51bacea4b83f8e6704
usamaGito <usamafassy@gmail.com>
2014-12-01 20:40:13 +0530

    Implemented packages update.

--- Codes/Code/web/admin/application/app/factories/packageTypeFactory.js
+++ Codes/Code/web/admin/application/app/factories/packageTypeFactory.js
@@ -9,24 +9,23 @@ App.factory('packageTypeFactory',function($resource){
     });
 
 	var factory = {}	
-	factory.getPackageTypes = function(){
-		alert('b');
+	factory.getPackageTypes = function(){		
 		return packageType.query()
 	}
 
 
-	// factory.savefixedAdvertisment = function($scope){
-	// 	return fixedAdvertisment.save($scope.fixedAdvertisment)
-	// 		.$promise.then(
-	// 			function(value){
-	// 				if(value.error!=true){
-	// 					window.location="#/fixedAdvertisment"
-	// 				}else{
-	// 					alert(value.message)	
-	// 				}
-	// 			}
-	// 		)
-	// 	}
+	factory.savePacakgeType = function($scope){
+		return packageType.save($scope.packageType)
+			.$promise.then(
+				function(value){
+					if(value.error!=true){
+						window.location="#/packageType"
+					}else{
+						alert(value.message)	
+					}
+				}
+			)
+		}
 
 	// factory.deleteFixedAdvertisment = function($scope,id){
 	// 	//userList.delete({id:id})
@@ -45,7 +44,7 @@ App.factory('packageTypeFactory',function($resource){
 	}
 
 	factory.updatePackageType = function($scope,id){		
-		return packageType.update({id:id},$scope.fixedAdsDetail.fixedAd[0])		
+		return packageType.update({id:id},$scope.packageTypesDetail.packageType[0])		
 	}
 
 
