66f316829ea8d5a8ac7148f6f4a32f7bcefec52d
deegha <shuboothi@gmail.com>
2014-12-05 19:40:58 +0530

    all fixed adds except categories left

--- Codes/Code/web/site/app/app/controllers/fixedAddsController.js
+++ Codes/Code/web/site/app/app/controllers/fixedAddsController.js
@@ -8,3 +8,18 @@ controllers.aftereventfixedAddsController = function($scope, fixedAddsFactory){
 	$scope.fixedAdd = fixedAddsFactory.getfixedAdd(5);	
 	
 }
+controllers.leftfixedAddsController = function($scope, fixedAddsFactory){
+
+	$scope.fixedAdd = fixedAddsFactory.getfixedAdd(2);	
+	
+}
+controllers.rightfixedAddsController = function($scope, fixedAddsFactory){
+
+	$scope.fixedAdd = fixedAddsFactory.getfixedAdd(3);	
+	
+}
+controllers.topfixedAddsController = function($scope, fixedAddsFactory){
+
+	$scope.fixedAdd = fixedAddsFactory.getfixedAdd(1);	
+	
+}
