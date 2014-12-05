636d8d88967a98f5289bef9be729bff4073560e9
usamaGito <usamafassy@gmail.com>
2014-12-05 07:57:03 +0530

    updated subscriberController.js with latest

--- Codes/Code/web/site/app/app/controllers/subscriberController.js
+++ Codes/Code/web/site/app/app/controllers/subscriberController.js
@@ -1,9 +1,6 @@
 controllers.subscriberController = function($scope, subscriberFactory){
 	$scope.addSubscriber = function(){
-		alert('i am here');
 		subscriberFactory.saveSubscriber($scope)
 	}
 }
-
-
 App.controller(controllers)
