873449dd15e69ee39c8ed3334fce9e33616855f0
usamaGito <usamafassy@gmail.com>
2014-12-05 07:31:07 +0530

    Inclution of subscriberFactory.js

---
+++ Codes/Code/web/site/app/app/factories/subscriberFactory.js
@@ -0,0 +1,31 @@
+//newsFactry
+
+App.factory('subscriberFactory',function($resource){
+	var subscriber = $resource('../../../api/subscriber/:id', {}, {
+        query: { method: 'GET', params: {}, isArray: false },
+        get: { method: 'GET', params: { id: '@id' } },
+        update: { method: 'PUT', params: { id: '@id' } },
+		save:{method: 'POST'},
+		delete:{method:'DELETE',params:{ id:'@id' }}
+    });
+	
+	
+	var factory = {}	
+	factory.subscriber = function(){		
+		return subscriber.query()
+	}
+
+
+	factory.saveSubscriber = function($scope){
+		return subscriber.save($scope.subscriber)
+			.$promise.catch(function(e){
+				alert(e.data.message)
+			}).then(function(value){
+				alert(value.message)
+				//$scope.subscriber=''
+			})
+		}
+	
+	
+	return factory
+})
