09d61e7abd3ed0dcdcbb92c147d4785ea680030f
usamaGito <usamafassy@gmail.com>
2014-12-07 11:27:31 +0530

    Implemented Add Post. (partial)

--- Codes/Code/web/site/app/app/controllers/advertismentController.js
+++ Codes/Code/web/site/app/app/controllers/advertismentController.js
@@ -4,3 +4,30 @@ controllers.addvertismentController= function($scope,advertismentFactory, $route
 	//$scope.temp = dd.advertisment[0].advertisement_attributes;
 	//alert($scope.adversiment);
 }
+
+controllers.advertismentAddController = function($scope, advertismentFactory){
+	// var uploader = $scope.uploader = new FileUploader({
+	// 	url: '../../../api/include/upload.php'
+	// })
+	console.log($scope);
+	$scope.addAdvertisment = function(){
+		// if(uploader.queue.length !=0){
+		// 	uploader.uploadAll()
+		// 	uploader.onCompleteItem = function(fileItem, response, status, headers) {	
+		// 		if(response.error==false){
+		// 			$scope.fixedAd.fixedads_image = response.image
+
+					advertismentFactory.saveAdvertisment($scope)
+		// 		}else{
+		// 			alert('image upload failed!')
+		// 		}
+		// 	};
+		// }else{
+		// 	alert('image should be selected')
+		// }
+		
+		
+		
+		
+	}
+}
