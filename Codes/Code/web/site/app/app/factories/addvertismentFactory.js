c8a013afa85784e056565f45ff8a35eac1ee5025
usamaGito <usamafassy@gmail.com>
2014-12-07 11:29:15 +0530

    Implemented saveadd (partial)

--- Codes/Code/web/site/app/app/factories/addvertismentFactory.js
+++ Codes/Code/web/site/app/app/factories/addvertismentFactory.js
@@ -1,15 +1,30 @@
 App.factory('advertismentFactory',function($resource){
-	var addList = $resource('../../../api/advertisment/:id', {}, {
+	var advertisment = $resource('../../../api/advertisment/:id', {}, {
         query: { method: 'GET', params: {}, isArray: false },
-        get: { method: 'GET', params: { id: '@id' } }
+        get: { method: 'GET', params: { id: '@id' } },
+        save:{method: 'POST'}
            });
+
 	var factory = {}
 	factory.getAdd = function(id){
-		return tld = addList.get({'id':id});
+		return tld = advertisment.get({'id':id});
 		tld.$promise.catch(function(e){
 			alert(e.data.message)
 		})
 	}
+
+
+	factory.saveAdvertisment = function($scope){	
+		
+		return advertisment.save($scope.adversiment)
+			.$promise.catch(function(e){
+				alert(e.data.message)
+			}).then(
+			function(value){
+				alert(value.message)
+				$scope.adversiment=''
+			})
+		}
 	return factory
 })
 
