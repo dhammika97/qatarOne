cdaa0813de6316e8d536c0fbc614b085a3f8f341
usamaGito <usamafassy@gmail.com>
2014-12-10 23:06:21 +0530

    updated settingFactory with correct API call URL.

--- Codes/Code/web/site/app/app/factories/settingFactory.js
+++ Codes/Code/web/site/app/app/factories/settingFactory.js
@@ -1,16 +1,16 @@
 App.factory('settingFactory',function($resource){
 	
 	var factory = {}
-	var userList = $resource('../../../api/user/:id', {}, {
-		get: { method: 'GET', params: { id: '@id' } }		
+	var userDetail = $resource('../../../api/GetUserDetail', {}, {
+			get: {  method: 'GET', params: {}, isArray: false  }	
     });
 
-	factory.getUser = function(id){
-		return user = userList.get({'id':id});
+	factory.getUser = function(){
+		return user = userDetail.get();
 		user.$promise.catch(function(e){
 			alert(e.data.message)
 		})
 	}
-		
 	return factory
 })
+
