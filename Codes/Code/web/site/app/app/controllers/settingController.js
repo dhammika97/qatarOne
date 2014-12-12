5eb244ef4f7fde0a9eeaaae93831eb18dd5b7c6b
usamaGito <usamafassy@gmail.com>
2014-12-12 06:27:58 +0530

    Updated the user profile with latest changes.

--- Codes/Code/web/site/app/app/controllers/settingController.js
+++ Codes/Code/web/site/app/app/controllers/settingController.js
@@ -1,4 +1,10 @@
-controllers.settingController = function($scope, settingFactory){
+controllers.settingController = function($scope, settingFactory, ngProgress, $timeout){
 	$scope.userSetting = settingFactory.getUser();	
+
+
+	$scope.updateUserSetting=function(id){
+		//alert($scope.user_id);
+		settingFactory.saveUserSetting($scope, id);
+	}
 }
 App.controller(controllers)
--- Codes/Code/web/site/app/app/factories/settingFactory.js
+++ Codes/Code/web/site/app/app/factories/settingFactory.js
@@ -11,6 +11,44 @@ App.factory('settingFactory',function($resource){
 			alert(e.data.message)
 		})
 	}
+
+	
+	var userProfile = $resource('../../../api/userProfile/:id', {}, {
+		 update: { method: 'PUT', params: { id: '@id' } }
+    });
+
+
+	factory.saveUserSetting = function($scope,id){
+
+		alert($scope.userSetting.user[0].user_confirmPassword);
+		//ngProgress.start()
+
+		if($scope.userSetting.user[0].user_New_password==$scope.userSetting.user[0].user_confirmPassword){			
+			return userProfile.update({id:id},$scope.userSetting.user[0])
+			.$promise.catch(function(e){
+				$scope.addAlert('danger',e.data.message)
+				ngProgress.complete()
+				$timeout(function(){
+					$scope.closeAlert();
+				}, 3000);
+			}).then(
+			function(value){
+				$scope.addAlert('success',value.message)
+				ngProgress.complete()
+				$timeout(function(){
+					$scope.closeAlert();
+					//$location.path('/packages-view')
+				}, 2000);
+			})	
+		}else{
+			alert('false');
+			$scope.addAlert('danger','Password mis-matched!')
+			ngProgress.complete()
+			$timeout(function(){
+				$scope.closeAlert();
+			}, 3000);
+		}
+	}
 	return factory
 })
 
