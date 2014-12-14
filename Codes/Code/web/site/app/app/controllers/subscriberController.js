2631348140cc1337d6f06ac31ff1c22fd8d0aec7
deegha <shuboothi@gmail.com>
2014-12-14 17:12:07 +0530

    mail-subscription

--- Codes/Code/web/site/app/app/controllers/subscriberController.js
+++ Codes/Code/web/site/app/app/controllers/subscriberController.js
@@ -2,5 +2,8 @@ controllers.subscriberController = function($scope, subscriberFactory){
 	$scope.addSubscriber = function(){
 		subscriberFactory.saveSubscriber($scope)
 	}
+	$scope.sendSubscribConfirmaton = function(){
+		subscriberFactory.sendMail($scope);	
+	}
 }
 App.controller(controllers)
--- Codes/Code/web/site/app/app/factories/subscriberFactory.js
+++ Codes/Code/web/site/app/app/factories/subscriberFactory.js
@@ -18,11 +18,19 @@ App.factory('subscriberFactory',function($resource){
 			.$promise.catch(function(e){
 				alert(e.data.message)
 			}).then(function(value){
+				$scope.sendSubscribConfirmaton();
 				alert(value.message)
 				//$scope.subscriber=''
 			})
 		}
-	
+	var mail = $resource('../../../api/mail/:id', {}, {
+        query: { method: 'POST', params: {}, isArray: false },
+
+    });
+    factory.sendMail = function($scope){
+
+    	mail.query({"fname":$scope.subscriber.subscription_email, "to":$scope.subscriber.subscription_email,"mailType":"subscribe"});
+    } 
 	
 	return factory
 })
